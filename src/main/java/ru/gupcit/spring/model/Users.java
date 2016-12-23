7cdc4e53fb9e455bc5bfe6ebeac65dcd32bab003
elbukaevzaur <elbukaevzaur@gmail.ru>
2016-12-23 09:30:00 +0300

    коммит для сохраниения

--- src/main/java/ru/gupcit/spring/model/Users.java
+++ src/main/java/ru/gupcit/spring/model/Users.java
@@ -4,4 +4,22 @@ package ru.gupcit.spring.model;
  * Created by zaur on 23.12.16.
  */
 public class Users {
+    private String username;
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
---
+++ src/main/java/ru/gupcit/spring/service/service.java
@@ -0,0 +1,7 @@
+package ru.gupcit.spring.service;
+
+/**
+ * Created by zaur on 23.12.16.
+ */
+public class service {
+}
